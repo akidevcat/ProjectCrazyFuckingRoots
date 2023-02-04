0ddf06caf2c593ebd1ab1631b90213060c7d3896
Ekaterina Urmanova <aleremualeremus@gmail.com>
2023-02-04 17:20:34 +0300

    AI dev

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    private Camera mainCamera;
+
+    public Transform moveToPoint;
+    void Start()
+    {
+        moveToPoint = GameObject.Find("Mother Tree").transform;
+        mainCamera = Camera.main;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(moveToPoint.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }*/
+    }
+
+    public void SetTarget(Transform target)
+    {
+        moveToPoint = target;
+    }
+}
